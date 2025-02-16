d97ffc4263b5f7026d9cfb47dd01d0192edb14c0
Sabbarbar2 <sabinconstantin60@yahoo.com>
2025-02-16 16:55:09 +0200

    finished the assignment

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,7 @@ import pygame # type: ignore
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
+        # groups logic
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -14,9 +14,16 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
+        # overrided by sub-classes
         pass
 
     def update(self, dt):
-        # sub-classes must override
+        # overrided by sub-classes
         pass
+    
+    def collision(self, otherCircle):
+        distance = self.position.distance_to(otherCircle.position) 
+        if distance <= (self.radius + otherCircle.radius):
+            return True
+        return False
+    
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import pygame # type: ignore
 from constants  import * 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -10,9 +13,16 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+        
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     dt = 0
     
@@ -22,6 +32,20 @@ def main():
                 return
         
         updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                return
+            
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+                    
+                
+            
+        
         
         screen.fill(color="black")
         
